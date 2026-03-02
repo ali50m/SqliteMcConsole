df5f33843557eef6e3adb140c47ce0b683ce02a7
liu <liu@book16>
2026-03-02 08:59:52 +0800

    增加了对比测试代码

--- src/SqliteMcConsoleNew/Program.cs
+++ src/SqliteMcConsoleNew/Program.cs
@@ -2,17 +2,23 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+// 核心：在任何数据库操作之前拦截并重定向驱动
+raw.SetProvider(new SQLite3Provider_e_sqlite3mc());
 
 var optionsBuilder = new DbContextOptionsBuilder<LibraryContext>();
 var connectionString = new SqliteConnectionStringBuilder
 {
     DataSource = "library.db",
-    Password = "my-password",
+    Password = "my-password", // SQLite3MC 会自动处理这个密钥
 }.ToString();
+
 optionsBuilder.UseSqlite(connectionString);
 
 using var context = new LibraryContext(optionsBuilder.Options);
 
+// 这里会创建数据库并应用加密密钥
 context.Database.EnsureCreated();
 
 await context.Books.AddAsync(new Book() { Title = Guid.NewGuid().ToString() });
@@ -20,6 +26,8 @@ await context.Books.AddAsync(new Book() { Title = Guid.NewGuid().ToString() });
 await context.SaveChangesAsync();
 
 Console.WriteLine(await context.Books.CountAsync());
+
+// --- 模型定义保持不变 ---
 
 public class LibraryContext(DbContextOptions<LibraryContext> options) : DbContext(options)
 {
